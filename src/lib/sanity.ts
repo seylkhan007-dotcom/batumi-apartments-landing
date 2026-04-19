25b18e1f5c1e5417c446ea0758ae001d0d493804
Хан <seylkhan007@gmail.com>
2026-04-19 19:38:16 +0500

    switch frontend to new sanity project

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,7 +1,7 @@
 import {createClient} from '@sanity/client'
 
 export const client = createClient({
-  projectId: 'sh3idnp7',
+  projectId: 'tsqh1ba0',
   dataset: 'production',
   apiVersion: '2024-01-01',
   useCdn: false,
